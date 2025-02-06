48533362ee16f604d86a089d366723aa6238ff44
Chirana <engsw.thiago@gmail.com>
2025-02-06 20:07:32 -0300

    feat: adjust dotfiles

--- nvim/nvim/lua/curumin/plugins/colorsheme.lua
+++ nvim/nvim/lua/curumin/plugins/colorsheme.lua
@@ -3,7 +3,7 @@ return {
 		"RRethy/base16-nvim",
 		priority = 1000,
 		init = function()
-			vim.cmd.colorscheme("base16-ia-dark")
+			vim.cmd.colorscheme("base16-ashes")
 			vim.cmd.hi("Comment gui=none")
 		end,
 	},
