120eb6ca7121c62e8cac499ca0b6b4ff3d5e4d17
Chirana <engsw.thiago@gmail.com>
2025-03-17 03:38:47 -0300

    feat: add new configs

--- arch.sh
+++ arch.sh
@@ -1,4 +1,5 @@
 
 
 sudo pacman -S libyaml git curl zsh vim neovim fontconfig nerd-fonts noto-fonts-emoji ttf-font-awesome eza bat mise pulseaudio pavucontrol polybar xclip
+sudo pacman -S noto-fonts-emoji noto-fonts noto-fonts-cjk ttf-liberation feh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
