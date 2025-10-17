d2432fcbda37ae15d40f55308cabfbed38962bd5
Chirana <engsw.thiago@gmail.com>
2025-10-17 04:35:09 -0300

    feat: add wifi verification

--- install.sh
+++ install.sh
@@ -8,23 +8,28 @@ sudo -v
 # Mantém a sessão sudo viva enquanto o script roda
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Pergunta Wi-Fi e senha
-read -rp "Nome do Wi-Fi: " WIFI
-read -rsp "Senha do Wi-Fi: " PASS
-echo
+# ==== CHECAR CONEXÃO ====
+echo "🔍 Verificando conexão com a internet..."
+if ping -q -c 1 -W 2 archlinux.org >/dev/null 2>&1; then
+  echo "🌐 Conexão detectada. Pulando configuração de Wi-Fi."
+else
+  echo "📶 Nenhuma conexão detectada. Configurando Wi-Fi..."
+  read -rp "Nome do Wi-Fi: " WIFI
+  read -rsp "Senha do Wi-Fi: " PASS
+  echo
 
-# ==== CONECTAR AO WI-FI ====
-sudo rfkill unblock all
-sudo rfkill unblock bluetooth
-sudo rfkill list bluetooth
-sudo rfkill unblock wifi
-sudo rfkill list wifi
+  sudo rfkill unblock all
+  sudo rfkill unblock bluetooth
+  sudo rfkill list bluetooth
+  sudo rfkill unblock wifi
+  sudo rfkill list wifi
 
-iwctl device list
-iwctl device wlan0 set-property Powered on
-iwctl station wlan0 scan
-iwctl station wlan0 get-networks
-iwctl --passphrase "$PASS" station wlan0 connect "$WIFI"
+  iwctl device list
+  iwctl device wlan0 set-property Powered on
+  iwctl station wlan0 scan
+  iwctl station wlan0 get-networks
+  iwctl --passphrase "$PASS" station wlan0 connect "$WIFI"
+fi
 
 # ==== PACMAN CONFIG ====
 sudo pacman -Syu --noconfirm
