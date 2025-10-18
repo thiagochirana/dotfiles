49d1d4014ac11d7881f69e6da0c35d737d2b3b47
Thiago Chirana <engsw.thiago@gmail.com>
2025-10-18 18:50:48 -0300

    feat: add tray and backslider

--- installers/utilities.sh
+++ installers/utilities.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S --needed --noconfirm alacritty mpv ghostty
 sudo pacman -S --needed --noconfirm impala htop btop fastfetch
-sudo pacman -S --needed --noconfirm chromium pulseaudio pavucontrol playerctl
+sudo pacman -S --needed --noconfirm chromium pavucontrol playerctl
 sudo pacman -S --needed --noconfirm nm-connection-editor bluez bluez-utils
 sudo pacman -S --needed --noconfirm brightnessctl upower
 yay -S --needed --noconfirm google-chrome elephant walker heroku-cli wlogout overskridez
