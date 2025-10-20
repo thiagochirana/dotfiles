8085f730409bb91dd4a34f4917c1da4a3ba4340c
Thiago Chirana <engsw.thiago@gmail.com>
2025-10-20 00:24:51 -0300

    feat: add run terminal

--- installers/utilities.sh
+++ installers/utilities.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm alacritty mpv ghostty mako
+sudo pacman -S --needed --noconfirm alacritty mpv ghostty mako xdotool jq
 sudo pacman -S --needed --noconfirm impala htop btop fastfetch
 sudo pacman -S --needed --noconfirm chromium pavucontrol playerctl
 sudo pacman -S --needed --noconfirm nm-connection-editor bluez bluez-utils
